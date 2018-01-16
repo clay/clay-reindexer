e73603997d487c06d61817b7bb33da7ef15cd0ab
Chris Kirk <cperryk@gmail.com>
2018-01-16 17:47:09 -0500

    More cleanup, more tests

--- index.js
+++ index.js
@@ -3,37 +3,54 @@
 const util = require('./lib/util'),
   reindexUtil = require('./lib/reindex-util'),
   args = require('yargs').argv,
-  client = require('./lib/es-client'),
   requireDirectory = require('require-directory'),
   runningAsScript = !module.parent;
 
+/**
+ * Confirm command-line args include all mandatory args.
+ * @param  {Object} args
+ * @return {Object} args
+ */
 function validateArgs(args) {
   if (!args.site) throw new Error('You must specify "site"');
   if (!args.elasticIndex) throw new Error('You must specify "elasticIndex"');
   if (!args.elasticHost) throw new Error('You must specify "elasticHost"');
+  if (!args.elasticPrefix) throw new Error('You must specify "elasticPrefix"');
+  return args;
 }
 
 /**
  * Re-indexes the specified site.
- * @param  {string} options.site
- * @param  {Object} options.client
- * @param  {string} options.elasticIndex
- * @param  {Object} [options.handlers]
+ * @param  {Object} opts
+ * @param  {string} opts.site
+ * @param  {string} opts.elasticIndex
+ * @param  {string} opts.elasticPrefix
+ * @param  {Object} [opts.handlers]
  * @return {Stream}
  */
-function reindexSite({site, elasticIndex, handlers}) {
-  return util.streamPageUris(site)
-    .flatMap(pageUri => reindexUtil.pageToDoc(pageUri, site, handlers))
-    .through(reindexUtil.putDocs(elasticIndex));
+function reindexSite(opts) {
+  return util.streamPageUris(opts.prefix)
+    .flatMap(pageUri => reindexUtil.pageToDoc(pageUri, opts))
+    .through(reindexUtil.putDocs(opts.elasticIndex));
+}
+
+/**
+ * Convert command-line args into programmatic options.
+ * @param  {Object} args
+ * @return {Object}
+ */
+function parseArgs(args) {
+  return {
+    prefix: args.site,
+    elasticIndex: args.elasticIndex,
+    elasticPrefix: args.elasticPrefix,
+    handlers: args.handlers &&
+      requireDirectory(module, args.handlers, {recurse: false})
+  };
 }
 
 function init() {
-  const {site, elasticIndex} = args,
-    handlers = requireDirectory(module, args.handlers, {recurse: false});
-
-  console.log('handlers');
-
-  reindexSite({site, client, elasticIndex, handlers})
+  reindexSite(parseArgs(args))
     .errors((error, push) => {
       const resultObj = {error, status: 'error'};
 
@@ -43,8 +60,6 @@ function init() {
     .tap(util.logResult())
     .done(() => process.exit());
 }
-
-validateArgs(args);
 
 if (runningAsScript) {
   validateArgs(args);
